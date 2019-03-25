7595d8868b1821481c65369b0a632e98a5bfb58f
fang0055 <fang0055@algonquinlive.com>
2019-03-24 22:17:53 -0400

    finished fetch profiles in js

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,24 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+let app = {
+    people: [],
+
+    init: function () {
+        app.ready();
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    ready: function () {
+        let url = "http://griffis.edumedia.ca/mad9022/tundra/get.profiles.php?gender=female"
+        fetch(url)
+            .then( (res) => { return res.json(); } )
+            .then( (data) => {
+                console.log(data.profiles);
+                app.people = app.people.concat(data.profiles);
+                console.log(app.people);
+            } )
     }
 };
 
-app.initialize();
+if ("cordova" in window){
+    document.addEventListener("deviceReady", app.init);
+} else{
+    document.addEventListener("DOMContentLoaded", app.init);
+}
