f9ebd4e5cfd37cec4be3ff27fb1c2361c7b04c13
fang0055 <fang0055@algonquinlive.com>
2019-03-28 14:01:22 -0400

    tested create card function and modified a little bit css

--- www/js/index.js
+++ www/js/index.js
@@ -12,10 +12,20 @@ let app = {
             .then( (res) => { return res.json(); } )
             .then( (data) => {
                 app.people = app.people.concat(data.profiles);
-                app.imageURL = data.imgBaseURL;
+                app.imageURL = decodeURIComponent(data.imgBaseURL);
+                localStorage.setItem("peopleKey", JSON.stringify(app.people));
                 console.log(app.imageURL);
                 console.log(app.people);
+                app.createCard();
             } )
+    },
+
+    createCard: function() {
+        document.querySelector(".pic").src = `${app.imageURL}${app.people[0].avatar}`;
+        document.querySelector(".nameText").textContent = app.people[0].last;
+        document.querySelector(".genderText").textContent = app.people[0].gender;
+        document.querySelector(".distanceText").textContent = app.people[0].distance;
+
     }
 };
 
