be6a1149ba4a948d00ac7a4ed5ec073fb971a796
fang0055 <fang0055@algonquinlive.com>
2019-03-28 14:46:57 -0400

    added basic save and delete fucntion

--- www/js/index.js
+++ www/js/index.js
@@ -1,31 +1,57 @@
 let app = {
     people: [],
+    savedPeople: [],
     imageURL: "",
+    tiny: "",
 
-    init: function () {
+    init: function (){
         app.ready();
+
+        tiny = new tinyshell(document.querySelector(".profileCtn"));
+        tiny.addEventListener("swiperight", app.saveOne);
+        tiny.addEventListener("swipeleft", app.deleteOne);
     },
 
-    ready: function () {
+    ready: function (){
         let url = "http://griffis.edumedia.ca/mad9022/tundra/get.profiles.php?gender=female"
         fetch(url)
             .then( (res) => { return res.json(); } )
             .then( (data) => {
                 app.people = app.people.concat(data.profiles);
                 app.imageURL = decodeURIComponent(data.imgBaseURL);
-                localStorage.setItem("peopleKey", JSON.stringify(app.people));
-                console.log(app.imageURL);
+                // localStorage.setItem("peopleKey", JSON.stringify(app.people));
                 console.log(app.people);
                 app.createCard();
             } )
+            .catch( (err) => {
+                console.log(err);
+            })
     },
 
-    createCard: function() {
+    createCard: function(){
         document.querySelector(".pic").src = `${app.imageURL}${app.people[0].avatar}`;
-        document.querySelector(".nameText").textContent = app.people[0].last;
+        document.querySelector(".nameText").textContent = app.people[0].first + " " + app.people[0].last;
         document.querySelector(".genderText").textContent = app.people[0].gender;
         document.querySelector(".distanceText").textContent = app.people[0].distance;
+        console.log(app.people);
+    },
 
+    saveOne: function(){
+        app.savedPeople.push(app.people[0]);
+        app.people.splice(0,1);
+        app.createCard();
+        if (app.people.length < 3){
+            app.ready();
+        }
+
+    },
+
+    deleteOne: function(){
+        app.people.splice(0,1);
+        app.createCard();
+        if (app.people.length < 3){
+            app.ready();
+        }
     }
 };
 
